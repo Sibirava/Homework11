37cde3ef6de73c76717570bc624f8d6e51ba8321
Admin <sibirava@gmail.com>
2022-11-09 08:33:05 +0300

    Task01, changed func according to SRP Task04, changed func according to SRP

--- Task04 Count odd_even_numbers in vector.py
+++ Task04 Count odd_even_numbers in vector.py
@@ -8,24 +8,28 @@ def random_vector_elements():
    vector = [random.randint(RANDOM_VALUE_DOWN, RANDOM_VALUE_UP) for i in range(NUMBER_VECTOR_ELEMENTS)]
    return vector
 
-def count_odd_even_elements(vector):
-    even_number = 0
+def count_odd_elements(vector):
     odd_number = 0
+
+    for i in vector:
+        if i % 2 == 1:
+            odd_number += 1
+    return odd_number
+
+def count_even_elements(vector):
+    even_number = 0
 
     for i in vector:
         if i % 2 == 0:
             even_number += 1
-        else:
-            odd_number += 1
-    return(even_number, odd_number)
+    return even_number
 
 
 def main():
     vector = random_vector_elements()
 
-    even_number, odd_number = count_odd_even_elements(vector)
-    msg = f"In vector{vector} there are {even_number} even numbers and \n" \
-          f"{odd_number} odd numbers."
+    msg = f"In vector{vector} there are {count_even_elements(vector)} even numbers and \n" \
+          f"{count_odd_elements(vector)} odd numbers."
     print(msg)
 
 main()
